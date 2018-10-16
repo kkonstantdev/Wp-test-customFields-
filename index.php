be0636f6e4c414798a60ac8274e86d42d6295f1b
Your Name <konstkad@gmail.com>
2018-10-16 20:08:25 +0300

    set in index

--- index.php
+++ index.php
@@ -55,6 +55,41 @@ get_header(); ?>
 		endif;
 		?>
 
+		<?php
+
+				// $terms = get_terms( 'taxnews' );
+
+				// if( $terms && ! is_wp_error($terms) ){
+				// 	echo "<ul>";
+				// 	foreach( $terms as $term ){
+				// 		echo "<li>". $term->name ."</li>";
+
+				// 	}
+				// 	echo "</ul>";
+				// }
+
+				$args = array( 'hide_empty=0' );
+					$terms = get_terms('taxnews', $args);
+
+					// собираем их и выводим
+					if ( !empty( $terms ) && !is_wp_error( $terms ) ) {
+						$count = count($terms);
+						$i=0;
+						$term_list = '<p class="taxnews-archive">';
+						foreach ($terms as $term) {
+							$i++;
+							$term_list .= '<a href="' . get_term_link( $term ) . '" title="' . sprintf(__('View all post filed under %s', 'my_localization_domain'), $term->name) . '">' . $term->name . '</a>';
+							if ($count != $i) {
+								$term_list .= ' &middot; ';
+							}
+							else {
+								$term_list .= '</p>';
+							}
+						}
+						echo $term_list;
+					}
+			?>
+
 		</main><!-- .site-main -->
 	</div><!-- .content-area -->
 
